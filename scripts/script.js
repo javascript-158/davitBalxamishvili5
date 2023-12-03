1a235a0e32f661bc0c1fed124b7fdaf029e227a4
dato201 <146935809+dato201@users.noreply.github.com>
2023-12-03 21:45:07 +0400

    add lesson5 dan

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+sayHello()
+
+// let promptFilter = prompt()
+
+
+// let textFilter = promptFilter.filter( person => person   > 6);
+
+// console.log(textFilter);
+
+let calculator = {
+    num1: 10,
+    num2: 5,
+    substract: function () {
+        return this.num1 - this.num2;
+    },
+    multiply: function () {
+        return this.num1 * this.num2;
+    }, 
+    divide: function () {
+        return this.num1 / this.num2;
+    }
+//     add: =
+};
+
+console.log(calculator.substract());
+console.log(calculator.multiply());
+console.log(calculator.divide());
+
+function sayHello() {
+    console.log("Hello world");  // ეს კონტენტი შექმნილი იყო 29 ზოლში მაგრამ გამოძახებულია პირველშივე ეს შესაძლებელია function ჰოისტინგის მეშვეობით.
+}                // სხვა შემთხვევაში არ იმუშავებს.
